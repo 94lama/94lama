76307a73bf63f98df9de4413b9250c168ccda2f9
Riccardo <riccardo.lamalfa94@gmail.com>
2026-04-12 15:30:28 +0000

    fix: load iubenda widget after hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
           id="iubenda-widget-bootstrap"
           src="https://embeds.iubenda.com/widgets/283fdcdd-8702-47d7-84ee-59bc1203b52c.js"
           type="text/javascript"
-          strategy="beforeInteractive"
         />
         {children}
         <LegalFooter />
