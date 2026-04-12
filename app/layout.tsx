3f31350c8ccf12969ff9f500043d8ba3ffa739ce
Riccardo <riccardo.lamalfa94@gmail.com>
2026-04-12 10:29:33 +0000

    feat: add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { LegalEmbedFooter } from "./components/legal-embed-footer";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,8 @@ export default function RootLayout({
       <body className="min-h-full bg-background text-foreground flex flex-col antialiased selection:bg-accent/30">
         {children}
         <LegalEmbedFooter />
+        {/* Google tag */}
+        <GoogleTagManager gtmId="G-Y55KQQ4S9Z" />
       </body>
     </html>
   );
