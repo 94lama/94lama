bffdf4369f6011cf6009448c4c8aab134c5f909f
Riccardo <riccardo.lamalfa94@gmail.com>
2026-05-04 16:24:40 +0200

    fix: update static builder settings

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,11 @@ import createNextIntPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Forces a static export
-  basePath: '/app', // Replace with your repository name
   images: {
     unoptimized: true, // Required because Next.js Image Optimization isn't supported on static hosts
   },
 };
 
-const withNextIntl = createNextIntPlugin("./app/i18n/request.ts");
+const withNextIntl = createNextIntPlugin("./i18n/request.ts");
 
 export default withNextIntl(nextConfig);
